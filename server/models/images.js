2732be25e3b273c5ec8075ed158a03f6e6878a73
Troy Dorman <troydorman@gmail.com>
2021-05-05 22:40:03 -0600

    implemented Images database model

---
+++ server/models/images.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const imageSchema = new mongoose.Scheuma({
+
+    name: {
+        type: String,
+        trim: true,
+        max: 160,
+        required: true
+    },
+    
+    description: {
+        type: String
+    },
+
+    img: {
+        data: Buffer,
+        contentType: String
+    },
+
+    user: {
+        type: String,
+        default: 'Admin'
+    }
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('Image', imageSchema)
