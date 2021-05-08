5e65f63afc65121f0d8c82e4e721864712f32559
Troy Dorman <troydorman@gmail.com>
2021-05-08 09:39:59 -0600

    added conditional rendering to our image display

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
-import React, {useState} from 'react'
-import FileUpload from './components/FilesUpload'
-import ShowPics from './components/ShowPics'
-
+import React, { useState } from "react";
+import FileUpload from "./components/FilesUpload";
+import ShowPics from "./components/ShowPics";
 
 const App = () => {
-    const [allPics, setAllPics] = useState([]);
-    return (
-        <div>
-            <FileUpload allPics={allPics} setAllPics={setAllPics}/>
-            <ShowPics allPics={allPics} setAllPics={setAllPics}/>
-            
-        </div>
-    )
-}
+  const [allPics, setAllPics] = useState([]);
 
-export default App
+  return (
+    <div>
+      <FileUpload allPics={allPics} setAllPics={setAllPics} />
+      {allPics.length > 0 ? (
+        <ShowPics allPics={allPics} setAllPics={setAllPics} />
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
+};
+
+export default App;
--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,5 +1,6 @@
 import imgModel from "../models/images";
-const upload = require("../services/image-upload");
+import upload from '../services/image-upload'
+
 const singleUpload = upload.single("image");
 
 export function getAllImages(req, res) {
@@ -14,48 +15,27 @@ export function getAllImages(req, res) {
 }
 
 export const postImage = (req, res, next) => {
-
-  
-
-
-  
-
   singleUpload(req, res, function (err) {
-
-   
     if (err) {
       return res.status(422).send({
         errors: [{ title: "Image Upload Error", detail: err.message }],
       });
     }
-
+    // create new imgModel db item with req.body.name, req.body.desc, and req.file.location     
     imgModel.create(
-        {
-          name: req.body.name,
-          desc: req.body.desc,
-          imgURL: req.file.location
-          
-        },
-        {},
-        (error, success) => {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log(success);
-          }
+      {
+        name: req.body.name,
+        desc: req.body.desc,
+        imgURL: req.file.location,
+      },
+      {},
+      (error, success) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(success);
         }
-      );
-
-    // create new imgModel db item with req.body.name, req.body.desc, and req.file.location
-
-    return res.json({ imageUrl: req.file.location });
+      }
+    );
   });
-
- 
-
- 
-
-  
-
-  
 };
